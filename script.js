4e855f07d890109b1218f7e1afb82b1605205dbc
pvestal <17632820+pvestal@users.noreply.github.com>
2018-10-11 08:41:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,90 @@
-var userChoice = prompt("Do you choose rock, paper, or scissors?");
+//This loads the first custom script after SP loads internal scripts
+_spBodyOnLoadFunctionNames.push("runLast");
 
-var computerChoice = Math.random();
+let userScore = 0;
+let computerScore = 0;
 
+let computerChoice = "";
+let userChoice = "";
 
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
-console.log("You chose: " + userChoice);
-console.log("Computer: " + computerChoice);
+function getComputerChoice() {
 
-var compare = function (choice1, choice2) {
+    computerChoice = Math.random(); //number between 0 and 1
 
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return "rock wins";
-        } else {
-            return "paper wins";
-        }
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 0.67) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
     }
-    else if (choice2 === "paper") {
-        if (choice2 === "rock") {
-            return "paper wins";
+    return computerChoice;
+}
+
+
+function runLast() {
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const scissors = document.getElementById('scissors');
+
+    rock.addEventListener('click', function () {
+        console.log("user selects rock.");
+        userChoice = "rock";
+        var pcChoice = getComputerChoice();
+        console.log("computer selects", pcChoice);
+        compare(userChoice, computerChoice);
+    });
+    paper.addEventListener('click', function () {
+        console.log("user selects paper.");
+        userChoice = "paper";
+        var pcChoice = getComputerChoice();
+        console.log("computer selects", pcChoice);
+        compare(userChoice, computerChoice);
+    });
+    scissors.addEventListener('click', function () {
+        console.log("user selects scissors.");
+        userChoice = "scissors";
+        var pcChoice = getComputerChoice();
+        console.log("computer selects", pcChoice);
+        compare(userChoice, computerChoice);
+    });
+}
+
+
+function compare(userChoice, computerChoice) {
+
+    if (userChoice === computerChoice) {
+        console.log("The result is a tie!");
+
+    } else if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
+            userScore++;
+            document.getElementById('userScore').innerHTML = userChoice;
+            console.log("rock wins");
+        } else {
+            computerScore++;
+            document.getElementById('computerScore').innerHTML = computerScore;
+            console.log("paper wins");
         }
-        else {
-            return "scissors win";
+    } else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            userScore++;
+            document.getElementById('userScore').innerHTML = userScore;
+            console.log("paper wins");
+        } else {
+            computerScore++;
+            document.getElementById('computerScore').innerHTML = computerScore;
+            console.log("scissors win");
+        }
+    } else if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore++;
+            document.getElementById('computerScore').innerHTML = computerScore;
+            console.log("rock wins");
+        } else {
+            userScore++;
+            document.getElementById('userScore').innerHTML = userScore;
+            console.log("scissors wins");
         }
     }
 };
-
-
-
-compare(userChoice, computerChoice);
